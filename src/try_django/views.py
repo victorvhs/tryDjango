fd7c25017ba1f429aefad5e432a2d448a0c3317f
Victor Reis <victor.h.s.reis@gmail.com>
2019-12-30 21:35:19 -0300

    template

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home_page(request):
-    return HttpResponse("<h1> Ta aqui</h1>")
+    return render(request,"hello_world.html")
 
 def about_page(request):
     return HttpResponse("<h1> Sobre</h1>")
